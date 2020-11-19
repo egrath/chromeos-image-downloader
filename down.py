0b21e9b65cf9d01d215902d2afeeb9264cad02dc
Egon Rath <egon.rath@gmail.com>
2020-11-19 19:48:51 +0100

    Added requirements.txt; Cleaned up code

--- down.py
+++ down.py
@@ -27,27 +27,43 @@ collections_response.ParseFromString(response.content)
 # iterate over the collections and fetch the list of images within each collection
 for c in collections_response.collections:
     print("Found collection:", c.collection_name)
-    
-    if not os.path.exists("output/"+c.collection_name):
-        os.mkdir("output/"+c.collection_name)
-    
+
+    # create the destination folder for the current collection
+    try:
+        if not os.path.exists("output/"+c.collection_name):
+            os.mkdir("output/"+c.collection_name)
+    except:
+        print("failed to create destination for collection")
+        continue
+
     # retrieve all images for the collection
-    backdrop_request=backdrop_wallpaper_pb2.GetImagesInCollectionRequest()
-    backdrop_request.collection_id = c.collection_id
-    backdrop_request.filtering_label.append("chromebook")
-    backdrop_request.filtering_label.append("google_branded_chromebook")
-    response=requests.post("https://clients3.google.com/cast/chromecast/home/wallpaper/collection-images?rt=b",headers={"Content-Type": "application/protobuf"},data=backdrop_request.SerializeToString())
-    
+    try:
+        backdrop_request=backdrop_wallpaper_pb2.GetImagesInCollectionRequest()
+        backdrop_request.collection_id = c.collection_id
+        backdrop_request.filtering_label.append("chromebook")
+        backdrop_request.filtering_label.append("google_branded_chromebook")
+        response=requests.post("https://clients3.google.com/cast/chromecast/home/wallpaper/collection-images?rt=b",headers={"Content-Type": "application/protobuf"},data=backdrop_request.SerializeToString())
+    except:
+        print("failed to download collection content for ["+c.collection_name+"]")
+        continue
+
     # parse the response and fetch each individual image
     images_response = backdrop_wallpaper_pb2.GetImagesInCollectionResponse()
     images_response.ParseFromString(response.content)
     for i in images_response.images:
-        full_url=i.image_url+"=s3840"
-        print("    downloading:", full_url)      
-        response = requests.get(full_url)
-        if response.status_code==200 and response.headers['Content-Type']=='image/jpeg':
-            destination="output/"+c.collection_name+"/"+str(i.asset_id)+".jpg"
-            output_file=open(destination,"wb")
-            output_file.write(response.content)
-            output_file.close()
-                    
+        full_url=i.image_url+"=s3840" # 4K resolution at max - most images retrieved are smaller
+        print("    downloading image with id:", i.asset_id, "("+i.attribution[0].text+")")
+        try:
+            response = requests.get(full_url)
+            if response.status_code==200 and response.headers['Content-Type']=='image/jpeg':
+                destination="output/"+c.collection_name+"/"+str(i.asset_id)+".jpg"
+                try:
+                    output_file=open(destination,"wb")
+                    output_file.write(response.content)
+                    output_file.close()
+                except:
+                    print("failed to save downloaded image ["+destination+"]")
+                    continue
+        except:
+            print("failed to download image from collection")
+            continue
