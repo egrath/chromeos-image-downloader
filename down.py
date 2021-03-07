673414feec6dfb07ec418924ca1dca97e728a22e
Egon Rath <egon.rath@gmail.com>
2021-03-07 20:09:55 +0100

    Added staging URL for Backdrops

--- down.py
+++ down.py
@@ -11,59 +11,71 @@
 #   protoc --python_out=.. backdrop_wallpaper.proto
 #
 
+import sys
 import os
 import requests
 import backdrop_wallpaper_pb2
 
-# create output directory
-if not os.path.exists("output"):
-    os.mkdir("output")
+if __name__=="__main__":
+    # constants
+    collections_url="https://clients3.google.com/cast/chromecast/home/wallpaper/collections?rt=b"
+    images_url="https://clients3.google.com/cast/chromecast/home/wallpaper/collection-images?rt=b"
 
-# fetch image collections
-response = requests.post("https://clients3.google.com/cast/chromecast/home/wallpaper/collections?rt=b", headers={"Content-Type": "application/protobuf"})
-collections_response = backdrop_wallpaper_pb2.GetCollectionsResponse()
-collections_response.ParseFromString(response.content)
+    # check command line arguments
+    for arg in sys.argv[1:]:
+        if arg=="--staging":
+            print("using alpha server instead of production")
+            collections_url=collections_url.replace("clients3","chromecast-staging.sandbox")
 
-# iterate over the collections and fetch the list of images within each collection
-for c in collections_response.collections:
-    print("Found collection:", c.collection_name)
+    # create output directory
+    if not os.path.exists("output"):
+        os.mkdir("output")
 
-    # create the destination folder for the current collection
-    try:
-        if not os.path.exists("output/"+c.collection_name):
-            os.mkdir("output/"+c.collection_name)
-    except:
-        print("failed to create destination for collection")
-        continue
+    # fetch image collections
+    response = requests.post(collections_url, headers={"Content-Type": "application/x-protobuf"})
+    collections_response = backdrop_wallpaper_pb2.GetCollectionsResponse()
+    collections_response.ParseFromString(response.content)
 
-    # retrieve all images for the collection
-    try:
-        backdrop_request=backdrop_wallpaper_pb2.GetImagesInCollectionRequest()
-        backdrop_request.collection_id = c.collection_id
-        backdrop_request.filtering_label.append("chromebook")
-        backdrop_request.filtering_label.append("google_branded_chromebook")
-        response=requests.post("https://clients3.google.com/cast/chromecast/home/wallpaper/collection-images?rt=b",headers={"Content-Type": "application/protobuf"},data=backdrop_request.SerializeToString())
-    except:
-        print("failed to download collection content for ["+c.collection_name+"]")
-        continue
+    # iterate over the collections and fetch the list of images within each collection
+    for c in collections_response.collections:
+        print("Found collection:", c.collection_name)
 
-    # parse the response and fetch each individual image
-    images_response = backdrop_wallpaper_pb2.GetImagesInCollectionResponse()
-    images_response.ParseFromString(response.content)
-    for i in images_response.images:
-        full_url=i.image_url+"=s3840" # 4K resolution at max - most images retrieved are smaller
-        print("    downloading image with id:", i.asset_id, "("+i.attribution[0].text+")")
+        # create the destination folder for the current collection
         try:
-            response = requests.get(full_url)
-            if response.status_code==200 and response.headers['Content-Type']=='image/jpeg':
-                destination="output/"+c.collection_name+"/"+str(i.asset_id)+".jpg"
-                try:
-                    output_file=open(destination,"wb")
-                    output_file.write(response.content)
-                    output_file.close()
-                except:
-                    print("failed to save downloaded image ["+destination+"]")
-                    continue
+            if not os.path.exists("output/"+c.collection_name):
+                os.mkdir("output/"+c.collection_name)
         except:
-            print("failed to download image from collection")
+            print("failed to create destination for collection")
             continue
+
+        # retrieve all images for the collection
+        try:
+            backdrop_request=backdrop_wallpaper_pb2.GetImagesInCollectionRequest()
+            backdrop_request.collection_id = c.collection_id
+            backdrop_request.filtering_label.append("chromebook")
+            backdrop_request.filtering_label.append("google_branded_chromebook")
+            response=requests.post(images_url,headers={"Content-Type": "application/x-protobuf"},data=backdrop_request.SerializeToString())
+        except:
+            print("failed to download collection content for ["+c.collection_name+"]")
+            continue
+
+        # parse the response and fetch each individual image
+        images_response = backdrop_wallpaper_pb2.GetImagesInCollectionResponse()
+        images_response.ParseFromString(response.content)
+        for i in images_response.images:
+            full_url=i.image_url+"=s3840" # 4K resolution at max - most images retrieved are smaller
+            print("    downloading image with id:", i.asset_id, "("+i.attribution[0].text+")")
+            try:
+                response = requests.get(full_url)
+                if response.status_code==200 and response.headers['Content-Type']=='image/jpeg':
+                    destination="output/"+c.collection_name+"/"+str(i.asset_id)+".jpg"
+                    try:
+                        output_file=open(destination,"wb")
+                        output_file.write(response.content)
+                        output_file.close()
+                    except:
+                        print("failed to save downloaded image ["+destination+"]")
+                        continue
+            except:
+                print("failed to download image from collection")
+                continue
